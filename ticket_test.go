9218d7d196fe73e50da4fefa2b16d5346e8d1862
Mega-Prime <67448214+Mega-Prime@users.noreply.github.com>
2020-08-18 10:33:45 -0400

    added logic to correct getByStatus

--- ticket_test.go
+++ ticket_test.go
@@ -65,6 +65,7 @@ func TestGetTicket(t *testing.T) {
 }
 
 func TestGetByStatus(t *testing.T) {
+	t.Parallel()
 	s := ticket.NewStore()
 	tkOpen := s.NewTicket("this is open")
 	tkClosed := s.NewTicket("this will be closed")
@@ -72,13 +73,28 @@ func TestGetByStatus(t *testing.T) {
 	want := []*ticket.Ticket{
 		tkOpen,
 	}
+	wantClosed := []*ticket.Ticket{
+		tkClosed,
+	}
 	got, err := s.GetByStatus(ticket.StatusOpen)
 	if err != nil {
 		t.Error(err)
 	}
-	if !cmp.Equal(want, got) {
-		t.Error(cmp.Diff(want, got))
+
+	gotclosed, err2 := s.GetByStatus(ticket.StatusClosed)
+	if err != nil {
+		t.Error(err2)
 	}
+	// if !cmp.Equal(want, got) {
+	// 	t.Error(cmp.Diff(want, got))
+	// }
+	if !cmp.Equal(want, got) {
+		t.Errorf("GetByStatus(StatusOpen): %v", cmp.Diff(want, got))
+	}
+	if !cmp.Equal(wantClosed, gotclosed) {
+		t.Errorf("GetByStatus(StatusOpen): %v", cmp.Diff(want, got))
+	}
+
 }
 
 func BenchmarkNewTicket(b *testing.B) {
